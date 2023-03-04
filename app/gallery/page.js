5795ea509aac641942dd9bc3a8194f5bbdcf575e
cristianv.ms <73314207+CristianMacha@users.noreply.github.com>
2023-03-03 22:06:20 -0500

    page services advance

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,7 +1,12 @@
 export default function GalleryPage() {
     return (
-        <div className="bg-slate-600 px-16 py-4">
-            <h1 className="text-white font-bold text-xl">GALERIA</h1>
-        </div>
+        <>
+            <div className="bg-slate-600 px-16 py-4">
+                <h1 className="text-white font-bold text-xl">GALERIA</h1>
+            </div>
+            <div className="px-16 py-4">
+
+            </div>
+        </>
     )
 }
---
+++ app/services/page.js
@@ -0,0 +1,67 @@
+export default function ServicesPage() {
+    return (
+        <>
+            <div className="bg-slate-600 px-16 py-4">
+                <h1 className="text-white font-bold text-xl">SERVICIOS</h1>
+            </div>
+            <div className="px-16 py-4 grid grid-cols-1">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    <div className="flex flex-col gap-2">
+                        <div>logo</div>
+                        <div className="text-center">
+                            <h2 className="text-vs-blue-600 font-semibold">DISEÑO DE LOGOTIPOS</h2>
+                            <p className="text-xs italic">Te diseñamos un logo de gran calidad estético y conceptual
+                                adaptada a las necesidades de
+                                tu negocio.</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <div>logo</div>
+                        <div className="text-center">
+                            <h2 className="text-vs-blue-600 font-semibold">DISEÑO DE LOGOTIPOS</h2>
+                            <p className="text-xs italic">Te diseñamos un logo de gran calidad estético y conceptual
+                                adaptada a las necesidades de
+                                tu negocio.</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <div>logo</div>
+                        <div className="text-center">
+                            <h2 className="text-vs-blue-600 font-semibold">DISEÑO DE LOGOTIPOS</h2>
+                            <p className="text-xs italic">Te diseñamos un logo de gran calidad estético y conceptual
+                                adaptada a las necesidades de
+                                tu negocio.</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <div>logo</div>
+                        <div className="text-center">
+                            <h2 className="text-vs-blue-600 font-semibold">DISEÑO DE LOGOTIPOS</h2>
+                            <p className="text-xs italic">Te diseñamos un logo de gran calidad estético y conceptual
+                                adaptada a las necesidades de
+                                tu negocio.</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <div>logo</div>
+                        <div className="text-center">
+                            <h2 className="text-vs-blue-600 font-semibold">DISEÑO DE LOGOTIPOS</h2>
+                            <p className="text-xs italic">Te diseñamos un logo de gran calidad estético y conceptual
+                                adaptada a las necesidades de
+                                tu negocio.</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <div>logo</div>
+                        <div className="text-center">
+                            <h2 className="text-vs-blue-600 font-semibold">DISEÑO DE LOGOTIPOS</h2>
+                            <p className="text-xs italic">Te diseñamos un logo de gran calidad estético y conceptual
+                                adaptada a las necesidades de
+                                tu negocio.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
